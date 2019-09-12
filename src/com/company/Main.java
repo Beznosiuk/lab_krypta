8d898fcb98ffdf2a6e722579f933bb9be7a64551
ira <besnosyuk>
2019-09-12 14:03:43 +0300

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import com.company.lab1.TestFerma;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Task1 task1 = new Task1();
+        TestFerma task1 = new TestFerma();
 
         task1.printArray(task1.createArray());
     }
