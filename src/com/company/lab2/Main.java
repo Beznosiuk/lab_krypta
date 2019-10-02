6a28ebc20a4542ceeabe8db21ba03cf9cd7c1b0c
Iryna Beznosiuk <Iryna_Beznosiuk@epam.com>
2019-10-02 22:29:36 +0300

    up

--- src/com/company/lab2/Main.java
+++ src/com/company/lab2/Main.java
@@ -10,38 +10,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random sc = new Random();
         Util util = new Util();
-
-        BigInteger p = new BigInteger(64, sc); //велике число
-//        BigInteger p = BigInteger.valueOf(103);
+//        BigInteger p = util.generateP();
+        BigInteger p = BigInteger.valueOf(5038849);
         BigInteger g = util.getPRoot(p); // первісний корінь
-//        BigInteger g = BigInteger.valueOf(4);
         BigInteger secretKey = new BigInteger(64, sc);
         BigInteger openKey = g.modPow(secretKey, p);
 
-        System.out.println("P = " + p + "\ng = " + g + "\nSecret key = " + secretKey + "\nOpen key = " + openKey);
+        System.out.println("P = " + p +
+                "\ng = " + g +
+                "\nSecret key = " + secretKey +
+                "\nOpen key = " + openKey);
 
         //encryption
         BigInteger k = new BigInteger(64, sc);
 
         System.out.println("Enter message: ");
-        BigInteger m = scanner.nextBigInteger();
 
+        BigInteger m = scanner.nextBigInteger();
         BigInteger firstPartOfMessage = g.modPow(k, p);
         BigInteger secondPart = m.multiply(openKey.modPow(k, p)).mod(p);
 
         System.out.println("Encrypted message: " + firstPartOfMessage + secondPart);
 
         //decryption
-
         BigInteger c1 = firstPartOfMessage.modPow(secretKey, p);
         BigInteger d = c1.modInverse(p);
         BigInteger result = d.multiply(secondPart).mod(p);
 
         System.out.println("Decrypted message: " + result);
-
-
-
     }
-
-
 }
--- src/com/company/lab2/Util.java
+++ src/com/company/lab2/Util.java
@@ -2,8 +2,38 @@ package com.company.lab2;
 
 import java.math.BigInteger;
 import java.util.HashSet;
+import java.util.Random;
 
 public class Util {
+
+    public BigInteger generateP() {
+        Random sc = new Random();
+
+        BigInteger p = new BigInteger(64, sc);
+        if (!isPrime(p)) {
+            p = new BigInteger(64, sc);
+        }
+        return p;
+    }
+
+    public boolean isPrime(BigInteger p) {
+        int divisorCounter = 1;
+        BigInteger index, i;
+
+        for (index = new BigInteger("2"); index.compareTo(p) != 1; index = index.add(new BigInteger("1"))) {
+//            System.out.println(index);
+            for (i = new BigInteger("2"); i.compareTo(index) != 1; i = i.add(new BigInteger("1"))) {
+                if ((p.mod(i).equals(BigInteger.ZERO))) {
+                    divisorCounter++;
+                }
+                if (divisorCounter > 2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public BigInteger getPRoot(BigInteger p) {
 
         for (BigInteger i = BigInteger.valueOf(0); i.compareTo(p) < 0; i = i.add(BigInteger.ONE)) {
@@ -16,6 +46,18 @@ public class Util {
 
 
     public boolean isPRoot(BigInteger p, BigInteger a) {
-        return a.compareTo(BigInteger.valueOf(0)) != 0 && a.compareTo(BigInteger.valueOf(1)) != 0;
+
+        if (a.compareTo(BigInteger.valueOf(0)) == 0 || a.compareTo(BigInteger.valueOf(1)) == 0) {
+            return false;
+        }
+        BigInteger last = BigInteger.valueOf(1);
+        HashSet<BigInteger> set = new HashSet<BigInteger>();
+        for (BigInteger i = BigInteger.valueOf(0); i.compareTo(p.subtract(BigInteger.valueOf(1))) < 0; i = i.add(BigInteger.ONE)) {
+            last = (last.multiply(a)).mod(p);
+            if (set.contains(last)) // Якщо повтор
+                return false;
+            set.add(last);
+        }
+        return true;
     }
 }
